3f56338b2b848800e242205d7d88aa4a406148e3
PARAS SHARMA <parassharma0220@gmail.com>
2024-09-10 00:12:31 +0530

    Update storeContext.jsx
    remove the at the end  / from the backend url

--- Frontend/src/context/storeContext.jsx
+++ Frontend/src/context/storeContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null)
 const storeContextProvider = (props) => {
 
     const [cartItems ,setCartItems ] = useState({});
-    const url = "https://sharmasweets-backend.onrender.com/"
+    const url = "https://sharmasweets-backend.onrender.com"
     const [token ,setToken] = useState("")
     const [food_list ,setFoodList] = useState([]);
 
